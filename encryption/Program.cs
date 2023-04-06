a10473a525b1614cea7cc6e5513821940b4198af
Anastasiiasyvak <asyvak@kse.org.ua>
2023-04-06 23:53:47 +0300

    dictionary for elements

--- encryption/Program.cs
+++ encryption/Program.cs
@@ -1,6 +1,18 @@
-﻿
+﻿Dictionary<char, int> our_elements = new Dictionary<char, int>();
 string pathtoFile = "/home/nastia/for_new_projects/encryption/our_file.txt";
-foreach (char el in File.ReadAllText(pathtoFile))
+foreach (char element in File.ReadAllText(pathtoFile))
 {
-    
+    if (our_elements.ContainsKey(element))
+    {
+        our_elements[element]++;
+    }
+    else
+    {
+        our_elements[element] = 1;
+    }
+}
+
+foreach (var pair in our_elements)
+{
+    Console.WriteLine("Key = {0}, Value = {1}", pair.Key, pair.Value);
 }
